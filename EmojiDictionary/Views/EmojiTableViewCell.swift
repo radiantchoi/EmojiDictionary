956d287ee9b8938309123903dfdb6a81db345688
Gordon Choi <thefirate@gmail.com>
2021-05-05 03:02:38 +0900

    Customizable cell setup

---
+++ EmojiDictionary/Views/EmojiTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by Gordon Choi on 2021/05/05.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    
+}
+
+extension EmojiTableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
